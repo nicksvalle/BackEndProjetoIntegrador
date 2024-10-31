b671dc36551c9879a9a7dca556f4a513ab081582
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2024-10-31 09:12:54 -0300

    fixing cors error

--- src/main/java/com/projeto_integrador/projeto_integrador/ProjetoIntegradorApplication.java
+++ src/main/java/com/projeto_integrador/projeto_integrador/ProjetoIntegradorApplication.java
@@ -2,6 +2,9 @@ package com.projeto_integrador.projeto_integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -12,5 +15,19 @@ public class ProjetoIntegradorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoIntegradorApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS em todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // Configura a origem permitida
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos os headers
+                        .allowCredentials(true); // Permite cookies e autenticação
+            }
+        };
+    }
 
 }
